702b8bce9e19532cc880aeb6cca04a82a82a8d95
GjermundStrøm <gjermundstroem@outlook.com>
2016-04-05 15:26:22 +0200

    master og versjon 0.1

--- SnuTekst/src/snuTekst/SnuTekst.java
+++ SnuTekst/src/snuTekst/SnuTekst.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 
 public class SnuTekst {
 	
-	public static void baklengs(String tekst){
+	public static void baklengs(String tekst, int teller){
 		System.out.print(tekst.charAt(tekst.length()-1));
 		if(tekst.length()>1)
-			baklengs(tekst.substring(0, tekst.length()-1));
+			baklengs(tekst.substring(0, tekst.length()-1),++teller);
+		else
+			System.out.println("\n");
 	}
 
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
-		Scanner input = new Scanner(System.in);
-		String tekst = input.nextLine();
-		System.out.print("\n"+ tekst + " er: ");
-		baklengs(tekst);
-		System.out.println(" baklengs");
+		while(true){
+			System.out.println("Skriv in tekst");
+			@SuppressWarnings("resource")
+			Scanner input = new Scanner(System.in);
+			String tekst = input.nextLine();
+			System.out.print("\n"+ tekst + " baklengs er: ");
+			baklengs(tekst,1);
+		}
 	}
 
 }
